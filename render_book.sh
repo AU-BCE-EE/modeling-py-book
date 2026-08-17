3dbfd20654ee6ef03afc7f8d24b3941124c86ad5
Sasha D. Hafner <sasha@hafnerconsulting.com>
2026-08-17 11:49:25 -0400

    use child process for rendering in script

--- render_book.sh
+++ render_book.sh
@@ -6,11 +6,12 @@ set -e
 
 repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Export pngs from xopp files using a forked child process
 echo "== Exporting xopp PNGs =="
 (cd "$repo_root/drawings" && ./xopp2png.sh)
 
 echo
+# And render book also using forked child process
 echo "== Rendering book =="
-
-quarto render
+(cd "$repo_root" && quarto render)
 
